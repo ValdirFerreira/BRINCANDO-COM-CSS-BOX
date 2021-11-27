1681b091e8b576f8da0facf3b8e4048ab18c3dc4
Valdir <valdir.silva@fcamara.com.br>
2021-11-27 01:25:04 -0300

    Brincando com css

---
+++ WebCss/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebCss.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
